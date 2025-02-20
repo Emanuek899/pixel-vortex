d2d971378d7db8fcd70ac5966c2f1964656fa482
Emanuel <emanuel.carbajalsantacurz@gmail.com>
2025-02-20 18:22:35 +0000

    Edicion login page

--- pixel_vortex/urls.py
+++ pixel_vortex/urls.py
@@ -23,7 +23,7 @@ from members import members_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", polls_views.index, name="index"),
-    path("login", members_views.login, name="login"),
-    path("catalogue", polls_views.catalogue, name="catalogue"),
+    path("login/", members_views.login, name="login"),
+    path("catalogue/", polls_views.catalogue, name="catalogue"),
     path("sign-up/", members_views.signup, name="signup")
 ]
