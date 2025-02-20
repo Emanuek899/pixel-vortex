85a2e001c2d50132ef5532b461b4122cf7c3d962
Gerardo <geruguz2003@gmail.com>
2025-02-20 09:15:47 -0600

    Acomodo Mamalon

--- members/members_views.py
+++ members/members_views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, "members/login.html")
+
+def signup(request):
+    return render(request, "members/signup.html")
