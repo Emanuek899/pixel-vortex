bbf56a62c43149270e1e8efb4783cf10d4e2bac4
Emanuel <emanuel.carbajalsantacruz@gmail.com>
2025-03-03 09:04:40 -0600

    <Update> update of apps models memberr and community

--- community/models.py
+++ community/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from polls.models import Genre
 from django.utils.translation import gettext_lazy as _
 from django.apps import apps
+from members.models import CustomUser
 
 # Create your models here.
 class Community(models.Model):
     community_id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey("members.CustomUser", related_name="users_community", on_delete=models.CASCADE)
     community_name = models.CharField(max_length=30, null=False, blank=False)
+    users = models.ManyToManyField(CustomUser, related_name="community_users", blank=True)
     users_count = models.IntegerField(default=0)
     creation_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(choices=[("A", "Active"), ("I", "Inactive")], max_length=20)
-    category = models.ForeignKey(Genre, related_name="categories", on_delete=models.CASCADE)
+    category = models.ForeignKey(Genre, related_name="categories", on_delete=models.SET_NULL, null=True, blank=True)
     
 
     class Meta:
@@ -19,6 +20,7 @@ class Community(models.Model):
         verbose_name_plural = _("Communities")
 
     def __str__(self):
+        users = [u.username for u in self.users.all()]
         return self.community_name
 
 
@@ -40,7 +42,7 @@ class Comment(models.Model):
 class Post(models.Model):
     post_id = models.BigAutoField(primary_key=True)
     user = models.ForeignKey("members.CustomUser", related_name="users_posts", on_delete=models.CASCADE)
-    community_id = models.ForeignKey(Community, related_name="posts", on_delete=models.CASCADE)
+    community = models.ForeignKey(Community, related_name="posts", on_delete=models.CASCADE)
     post_title = models.CharField(max_length=20, null=False, blank=False)
     post_description = models.CharField(max_length=100, null=False, blank=False)
     post_date = models.DateTimeField(auto_now_add=True)
