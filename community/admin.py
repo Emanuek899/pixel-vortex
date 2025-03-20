2d5a3fffcddcafd900a41438c09c070fa784a661
Emanuel <emanuel.carbajalsantacruz@gmail.com>
2025-03-19 19:26:39 -0600

    <Edition> miscelaneous files (admin, settings, static)

--- community/admin.py
+++ community/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from community.models import Comment, Community, Post
+from community.models import Comment, Community, Post, Like
 
 # Register your models here.
 admin.site.register(Community)
 admin.site.register(Comment)
 admin.site.register(Post)
+admin.site.register(Like)
