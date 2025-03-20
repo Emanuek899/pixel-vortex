de6fb9521ed35ce53649a51f2626f54a35d0c52e
Emanuel <emanuel.carbajalsantacruz@gmail.com>
2025-03-19 19:19:39 -0600

    <Edition> Community views and api implementation

--- community/views.py
+++ community/views.py
@@ -1,48 +1,64 @@
-from django.shortcuts import render, get_object_or_404, redirect
+"""
+This module controls all the options over the community section
+such show communities, join communities etc...
+"""
 from . import models
-from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets, filters
+from . import serializers
+from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+
 
 # Create your views here.
-@login_required
-def showCommunities(request):
-    # Get all the communities and give it to the page as context
-    community = models.Community.objects.all()
-    return render(request, "community/community.html", {"communities": community})
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Api endpoint that allow posts to be viewed or edited
+    """
+    queryset = models.Post.objects.all().order_by("-created_at")
+    serializer_class = serializers.PostSerializer
 
-@login_required
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+
+class CommunityViewSet(viewsets.ModelViewSet):
+    """
+    Api endpoint for communities
+    """
+    queryset = models.Community.objects.all().order_by('-creation_date')
+    serializer_class = serializers.CommunitySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ['community_name']
+
+    def get_queryset(self):
+        """
+        This function allows to get all the coincidences with
+        the search term and return a queryset
+        """
+        queryset = super().get_queryset()
+        search_term = self.request.query_params.get('search', None)
+
+        if search_term:
+            queryset = queryset.filter(community_name__icontains=search_term)
+            # Aquí se realiza la búsqueda case-insensitive
+        return queryset
+
+
+def communitiesList(request):
+    """
+    Show all the communities
+    """
+    return render(request, 'community/communities.html')
+
+
 def community(request, community_id):
-    # Check if a community exist (is and instance of Community) by id
-    community = get_object_or_404(models.Community, community_id=community_id)
-    posts = models.Post.objects.filter(community=community)
-    return render(request,"community/community_detail.html", {"community": community, "posts":posts})
-
-
-@login_required
-def joinCommunity(request, community_id):
-    # Get the comuunity bi his id
-    community= get_object_or_404(models.Community, community_id=community_id)
-
-    # Checks if the user is not in the community, if not in, then he can join
-    if request.user not in community.users.all():
-        community.users.add(request.user)
-        community.users_count = community.users.count()
-        community.save()
-    
-    return redirect("community", community_id=community_id)
-
-@login_required
-def makePost(request, community_id):
-    community = get_object_or_404(models.Community, community_id=community_id)
-
-    if request.method == "POST":
-        post_title = request.POST.get("post_title")
-        post_description = request.POST.get("post_description")
-
-        if post_title and post_description:
-            post = models.Post.objects.create(
-                user = request.user,
-                community = community,
-                post_title = post_title,
-                post_description = post_description, 
-            )
-            return redirect("community", community_id=community_id)
+    """
+    Show a specific community
+    """
+    context = {
+        'community_id': community_id
+    }
+    return render(request, 'community/community.html', context)
